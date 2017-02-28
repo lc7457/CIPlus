f61d3a7da4644970487000a1d663ff714685b651
JDZ.Chao <jdzchao@gmail.com>
2017-02-28 18:07:02 +0800

    add API controllers extends thanks for https://github.com/chriskacerguis/codeigniter-restserver

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,15 +1,13 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
-    public function Index()
+    public function index()
     {
-        $this->load->view('welcome_message');
-    }
+        $this->load->helper('url');
 
+        $this->load->view('rest_server');
+    }
 }
