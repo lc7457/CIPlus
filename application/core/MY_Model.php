65856db94079e36bc7cbdd81003d73ce67d997cc
JDZ.Chao <jdzchao@gmail.com>
2018-07-14 15:00:37 +0800

    1.1.0 更新新的model管理模式

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -1,197 +1,115 @@
 <?php defined('BASEPATH') or exit ('No direct script access allowed');
 
-/**
- * 数据库CRUD操作扩展(models)
- * ============================================================
- * 简化数据库CRUD基本功能操作
- * insert : 插入单条数据（返回执行数据插入时的ID）
- * update : 更新数据（返回成功更新的数据行数）
- * delete : 删除数据（返回被删除的数据行数）
- * row : 查询单条数据（返回结果集数组）
- * result : 查询多条数据（返回结果集二维数组）
- * count : 查询数据条数（返回查询数据行数）
- *
- * @author LeeNux
- * @version 1.0
- */
 abstract class MY_Model extends CI_Model {
-    // 数据表名称
-    protected $table;
     
-    /**
-     * 构造函数
-     * MY_Model constructor.
-     * @param string $table
-     * @param string $database
-     */
-    public function __construct($table = '', $database = 'default') {
+    public function __construct($database = 'default') {
         parent::__construct();
         $this->db = $this->load->database($database, TRUE);
-        $this->table = $table;
-    }
-    
-    /**
-     * 查询字段
-     * @param string $selected
-     * @return $this
-     */
-    public function select($selected = '*') {
-        $this->db->select($selected);
-        return $this;
-    }
-    
-    /**
-     * 排序条件
-     * @param string $by
-     * @return $this
-     */
-    public function order($by = '') {
-        if (!empty($by)) {
-            $by = str_replace("@", " ", $by);
-            $this->db->order_by($by);
-        }
-        return $this;
-    }
-    
-    /**
-     * 模糊匹配字段
-     * @param $column
-     * @param string $string
-     * @param string $type
-     * @return $this
-     */
-    public function like($column, $string = '', $type = 'both') {
-        if (is_array($column)) {
-            $this->db->like($column);
-        } else {
-            $this->db->like($column, $string, $type);
-        }
-        return $this;
-    }
-    
-    /**
-     * 模糊匹配字段
-     * @param $column
-     * @param string $string
-     * @param string $type
-     * @return $this
-     */
-    public function or_like($column, $string = '', $type = 'both') {
-        if (is_array($column)) {
-            $this->db->or_like($column);
-        } else {
-            $this->db->or_like($column, $string, $type);
-        }
-        return $this;
-    }
-    /**
-     * 匹配多条数据
-     * @param $key
-     * @param array $array
-     * @return $this
-     */
-    public function where_in($key, array $array = array()) {
-        $this->db->where_in($key, $array);
-        return $this;
-    }
-    /**
-     * 分组处理
-     * @param $key
-     * @return $this
-     */
-    public function group_by($key) {
-        $this->db->group_by($key);
-        return $this;
     }
     
     /**
      * 替换数据
+     * @param $table
      * @param array $dataArr
      * @return mixed
      */
-    public function replace($dataArr = array()) {
-        return $this->db->replace($this->table, $dataArr);
+    protected function replace($table, array $dataArr) {
+        return $this->db->replace($table, $dataArr);
     }
     
     /**
      * 添加数据
+     * @param $table
      * @param array $dataArr
-     * @return mixed
+     * @return mixed 插入的id
      */
-    public function insert($dataArr = array()) {
-        $this->db->insert($this->table, $dataArr);
+    protected function insert($table, array $dataArr) {
+        $this->db->insert($table, $dataArr);
         return $this->db->insert_id();
     }
     
     /**
      * 修改数据
+     * @param $table
      * @param $dataArr
      * @param $whereArr
-     * @return mixed
+     * @return mixed 影响的行数
      */
-    public function update($dataArr, $whereArr) {
+    protected function update($table, $dataArr, $whereArr) {
         $this->db->where($whereArr);
-        $this->db->update($this->table, $dataArr);
+        $this->db->update($table, $dataArr);
         return $this->db->affected_rows();
     }
     
     /**
      * 删除数据
+     * @param $table
      * @param $whereArr
-     * @return mixed
+     * @return mixed 影响的行数
      */
-    public function delete($whereArr) {
+    protected function delete($table, $whereArr) {
         $this->db->where($whereArr);
-        $this->db->delete($this->table);
+        $this->db->delete($table);
         return $this->db->affected_rows();
     }
     
     /**
      * 查询并返回一条数据
-     * @param $whereArr
-     * @return mixed
+     * @param $table
+     * @return mixed 数据数组
      */
-    public function row($whereArr) {
-        $this->db->where($whereArr);
-        $query = $this->db->get($this->table);
+    protected function row($table) {
+        $query = $this->db->get($table);
         return $query->row_array();
     }
     
     /**
      * 查询并返回多条数据
-     * @param array $whereArr
+     * @param $table
      * @param int $page
      * @param int $num
-     * @return mixed
+     * @return mixed 数据数组集
      */
-    public function result($whereArr = array(), $page = 1, $num = 10) {
+    protected function result($table, $page = 1, $num = 10) {
         if ($page <= 0) $page = 1;
         $offset = ($page - 1) * $num;
-        $this->db->where($whereArr);
-        $query = $this->db->get($this->table, $num, $offset);
+        $query = $this->db->get($table, $num, $offset);
         return $query->result_array();
     }
     
     /**
      * 查询并返回全部数据
-     * @param array $whereArr
+     * @param $table
      * @return mixed
      */
-    public function result_all($whereArr = array()) {
-        $this->db->where($whereArr);
-        $query = $this->db->get($this->table);
+    protected function result_all($table) {
+        $query = $this->db->get($table);
         return $query->result_array();
     }
     
     /**
      * 查询数据总数
-     * @param $whereArr
+     * @param $table
      * @return mixed
      */
-    public function count($whereArr = array()) {
-        $this->db->where($whereArr);
-        $query = $this->db->get($this->table);
+    protected function count($table) {
+        $query = $this->db->get($table);
         return $query->num_rows();
+    }
+    
+    /**
+     * 清除所有数据缓存
+     */
+    public function cache_clear() {
+        $this->db->cache_delete_all();
+    }
+    
+    /**
+     * 重置查询条件缓存
+     */
+    public function query_reset() {
+        $this->db->reset_query();
+        $this->db->flush_cache();
     }
     
 }
